e22086bb8dc686036f634ae56e45cfd6ad4a747f
MMizogaki <voice.hito@gmail.com>
2015-10-16 16:59:03 +0900

    Initial Commit

---
+++ JRHttpSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JRHttpSample
+//
+//  Created by 溝垣雅人 on 10/16/15.
+//  Copyright © 2015 [MMasahito](https://github.com/MMasahito). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
